adee6efdf47d4fc4c2cbdc0711849234794dbd57
yanikisia <kathlyn.yary@gmail.com>
2021-11-05 19:43:08 -0300

    listinha

--- modulo2/estados-insta4/insta4/insta4/src/App.js
+++ modulo2/estados-insta4/insta4/insta4/src/App.js
@@ -8,27 +8,80 @@ const MainContainer = styled.div`
   flex-direction: column;
   align-items: center;
 `
-
+const PostAdicionado=styled.div`
+  display:flex;
+  flex-direction: column;
+  margin-bottom: 10px;
+  
+  input{
+    margin-bottom:4px;
+  }
+`
 class App extends React.Component {
+  state={
+    inputUsuario:"",
+    inputFotoPerfil:"",
+    inputFotoPost:"",
+
+    post:[
+      {  
+      nomeUsuario:'paulinha',
+      fotoUsuario:'https://picsum.photos/50/50',
+      fotoPost:'https://picsum.photos/200/150'
+    },
+    { 
+      nomeUsuario:'Ana julia',
+      fotoUsuario:'https://picsum.photos/50/50?a=2',
+      fotoPost:'https://picsum.photos/200/150?a=4',
+    },
+    { nomeUsuario:'Zéziho',
+      fotoUsuario:'https://picsum.photos/50/50?a=1',
+      fotoPost:'https://picsum.photos/200/150?a=5'
+    },
+    ]
+}
+  guardarNome=(event)=>{
+    this.setState({ inputUsuario: event.target.value })
+  }
+  guardarFotoUsuario=(event)=>{
+    this.setState({ inputFotoPerfil: event.target.value })
+  }
+  guardarFotoPost=(event)=>{
+    this.setState({ inputFotoPost: event.target.value })
+  }
+  adicionarPost=()=>{
+    const novoPost={
+      nomeUsuario:this.state.inputUsuario,
+      fotoUsuario:this.state.inputFotoPerfil,
+      fotoPost:this.state.inputFotoPost
+    }
+    const copia=[...this.state.post,novoPost]
+    this.setState({post:copia})
+    this.setState({ inputUsuario: "", inputFotoPerfil: "", inputFotoPost:""});
+  }
   render() {
+    const componentes=this.state.post.map((p,index)=>{
+      return (
+        <Post key={index}
+          nomeUsuario={p.nomeUsuario}
+          fotoUsuario={p.fotoUsuario}
+          fotoPost={p.fotoPost}
+        />
+      )
+    })
+    console.log(componentes.nomeUsuario)
     return (
-      <MainContainer>
-        <Post
-          nomeUsuario={'paulinha'}
-          fotoUsuario={'https://picsum.photos/50/50'}
-          fotoPost={'https://picsum.photos/200/150'}
-        />
-        <Post
-          nomeUsuario={'Ana julia'}
-          fotoUsuario={'https://picsum.photos/50/50?a=2'}
-          fotoPost={'https://picsum.photos/200/150?a=4'}
-        />
-        <Post
-          nomeUsuario={'Zéziho'}
-          fotoUsuario={'https://picsum.photos/50/50?a=1'}
-          fotoPost={'https://picsum.photos/200/150?a=5'}
-        />
-      </MainContainer>
+     <MainContainer>
+       {componentes}
+       <PostAdicionado>
+       <input  placeholder="coloque o seu nome " value={this.state.inputUsuario} onChange={this.guardarNome} />
+       <input value={this.state.inputFotoPost} onChange={this.guardarFotoPost} placeholder={"coloque um url para o  post"}/>
+       <input value={this.state.inputFotoPerfil} onChange={this.guardarFotoUsuario} placeholder={"coloque um url para o perfil"}/>
+       <button onClick={this.adicionarPost} >Adicionar post</button>
+       </PostAdicionado>
+     </MainContainer>
+   
+      
     );
   }
 }
