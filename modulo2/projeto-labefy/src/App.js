169533ba95aaadd75dba90806b82b5ddf9c92d9d
yanikisia <kathlyn.yary@gmail.com>
2021-11-18 16:51:10 -0300

    titulo

---
+++ modulo2/projeto-labefy/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import styled from 'styled-components';
+ 
+ const AppContainer=styled.div` 
+  text-align: center;
+ `
+ class App extends React.Component {
+  render(){
+    return (
+      <AppContainer>
+        <h2>Labefy</h2>
+      </AppContainer>
+    )
+  }
+
+}
+export default App
+
