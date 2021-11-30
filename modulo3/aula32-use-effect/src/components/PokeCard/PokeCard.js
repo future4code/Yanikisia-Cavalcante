5f22185337b0e60a1d81e4593ad4605685ddf853
yanikisia <kathlyn.yary@gmail.com>
2021-11-30 16:22:11 -0300

    os arquivos foi mudado de componente de classe para componente funcionais, tambem foi adicionado a hooks useEffect no lugar do componentDidmount

---
+++ modulo3/aula32-use-effect/src/components/PokeCard/PokeCard.js
@@ -0,0 +1,39 @@
+import React, { useState,useEffect } from "react";
+import axios from "axios";
+
+
+export default function PokeCard(props){
+ const [pokemonInfo,setPokemonInfo]=useState({})
+
+ useEffect(()=>{
+    pegarPokemon(props.pokemon)
+ },[pokemonInfo])
+
+ const pegarPokemon=(pokeName)=>{
+     axios
+     .get(`https://pokeapi.co/api/v2/pokemon/${pokeName}`)
+     .then((res)=>{
+         setPokemonInfo(res.data)
+         console.log(res.data)
+     })
+     .catch((err)=>{
+         console.log(err)
+     })
+ }
+
+ const pokemon=pokemonInfo
+ console.log(pokemon)
+ return(
+    <div>
+         <p>{pokemon.name}</p>
+        <p>{pokemon.weight} Kg</p>
+        {pokemon.types && <p>{pokemon.types[0].type.name}</p>}
+        {pokemon.sprites && (
+          <img src={pokemon.sprites.front_default} alt={pokemon.name} />
+        )}
+    </div>
+
+ )
+
+
+}
