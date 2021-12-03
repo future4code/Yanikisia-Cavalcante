9925085ddddb8fa866067ab1e9420c4e8a9c8d43
yanikisia <kathlyn.yary@gmail.com>
2021-12-03 15:30:00 -0300

    criado as funcionalidades, todas funçoes, estilização

--- modulo3/projeto-astromatch/src/App.js
+++ modulo3/projeto-astromatch/src/App.js
@@ -1,22 +1,101 @@
 import styled from 'styled-components';
 import MatchCard from './components/MatchCard';
-const AppContainer=styled.div`
+import React, { useState, useEffect } from 'react';
+import { getProfiles, chosseMatch, getMatches, getClear } from "../src/components/Actions"
+import MatchesScreen from './components/MatchesScreen';
+
+const AppContainer = styled.div`
 *{
   margin: 0;
   padding: 0;
 }
-display: flex;
-justify-content: center;
-align-items: center;
-width: 100vw;
-height: 100vh;
-text-align: center;
-/* background-color: grey; */
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100vw;
+  height: 100vh;
+  text-align: center;
+  background-color: grey;
+  position: absolute;
+  z-index: -1;
+
 `
+const DivInformaçoes = styled.div`
+  background-color: white;
+  width: 400px;
+  height: 90vh;
+  z-index: -1;
+  border-radius: 10px;
+`
+const DivInfor = styled.div`
+  width: 100%;
+  height: 75vh;
+`
+
 function App() {
+  const [possibleMatch, setPossibleMatch] = useState({})
+  const [matches, setMatches] = useState([])
+  const [idMatch, setIdMatch] = useState("")
+  const [screen, setScreen] = useState("atches")
+  const [loading, setLoading] = useState(true)
+
+  const getProfile = () => {
+    console.log(getProfiles())
+    const profile = getProfiles()
+    profile.then((res) => {
+      if (res.profile === null) {
+        return {}
+      }
+      else {
+        setPossibleMatch(res.profile || {})
+        setIdMatch(res.profile.id)
+      }
+      return profile
+    })
+  }
+
+  const callGetMatches = () => {
+    const match = getMatches()
+    match.then((res) => {
+      setMatches(res.matches || [])
+      return match
+    })
+  }
+
+  const changeMatches = () => {
+    getClear()
+    setMatches([])
+  }
+  useEffect(() => {
+    getProfile()
+  }, [])
+
+  const changeScreens = () => {
+    switch (screen) {
+
+      case "telaMatches":
+        return <MatchesScreen getPro={getProfile} clear={changeMatches} callingScreen={callScreen} getMatch={callGetMatches} match={matches} />
+      default:
+        return <MatchCard callingScreen={callScreen} possibles={possibleMatch} choose={chosseMatch} id={idMatch} getPro={getProfile} />
+    }
+  }
+
+  const callScreen = (id) => {
+    setScreen(id)
+    sessionStorage.setItem('tela', id)
+    changeScreens()
+  }
+
+
   return (
     <AppContainer>
-      <MatchCard/>
+
+      < DivInformaçoes>
+        <DivInfor>
+          {changeScreens()}
+        </DivInfor>
+      </ DivInformaçoes>
+
     </AppContainer>
   );
 }
--- modulo3/projeto-astromatch/src/components/Actions.js
+++ modulo3/projeto-astromatch/src/components/Actions.js
@@ -1,24 +1,13 @@
 import axios from "axios";
-const aluno="yanikisia"
- 
+import PulseLoader from "react-spinners/PulseLoader";
+const aluno = "yanikisia"
 
-// export const getProfiles = () => {
-//     axios.get(`https://us-central1-missao-newton.cloudfunctions.net/astroMatch/${aluno}/person`)
-//    .then((res)=>{
-//         console.log(res.data)
-//     })
-//     .catch((err) => {
-//         console.log(err.message)
-//     })
-// }
 
 
 export const getProfiles = async () => {
 
     try {
-        const request= await axios.get(`https://us-central1-missao-newton.cloudfunctions.net/astroMatch/${aluno}/person`)
-        console.log(request.data)
-     
+        const request = await axios.get(`https://us-central1-missao-newton.cloudfunctions.net/astroMatch/${aluno}/person`)
         return request.data
 
     } catch (err) {
@@ -26,14 +15,36 @@ export const getProfiles = async () => {
     }
 }
 
-// const getProfiles=()=>{
-//     axios.get(`https://us-central1-missao-newton.cloudfunctions.net/astroMatch/${aluno}/person`)
-   
-    
-//         console.log(res.data)
-//     })
-//     .catch((err) => {
-//         console.log(err.message)
-//     })
-// }
+export const chosseMatch = (body, funcao) => {
+    console.log()
+    axios.post(`https://us-central1-missao-newton.cloudfunctions.net/astroMatch/${aluno}/choose-person`, body)
+        .then((res) => {
+            console.log(res.data.isMatch)
+            funcao()
+        }).catch((err) => {
+            console.log(err)
+        })
+}
 
+export const getMatches = async () => {
+
+    try {
+        const request = await axios.get(`https://us-central1-missao-newton.cloudfunctions.net/astroMatch/${aluno}/matches`)
+        console.log(request.data)
+        return request.data
+
+    }
+    catch (err) {
+        console.log(err.message)
+    }
+}
+
+export const getClear = () => {
+    axios.put(`https://us-central1-missao-newton.cloudfunctions.net/astroMatch/${aluno}/clear`)
+        .then((res) => {
+            console.log(res.data)
+            getProfiles()
+        }).catch((err) => {
+            console.log(err)
+        })
+}
