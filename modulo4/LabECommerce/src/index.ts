bdd710decf84a9dc9d0d8486e68777bd0724c104
yanikisia <kathlyn.yary@gmail.com>
2022-02-11 22:44:29 -0300

    endpoint de registro de compra feito

--- modulo4/LabECommerce/src/index.ts
+++ modulo4/LabECommerce/src/index.ts
@@ -3,8 +3,10 @@ import { createUsers } from "./endpoints/createUsers";
 import {getAllUsers} from "./endpoints/getAllUsers"
 import {createProducts} from "./endpoints/createProducts"
 import { getAllProducts } from "./endpoints/getAllProducts";
+import { shoppingRegister } from "./endpoints/shoppingRegister";
 
 app.post("/users",createUsers);
 app.post("/products",createProducts);
+app.post("/purchases",shoppingRegister);
 app.get("/users",getAllUsers);
 app.get("/products",getAllProducts);
