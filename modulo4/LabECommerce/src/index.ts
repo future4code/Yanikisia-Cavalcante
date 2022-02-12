703ce6488110320a038bf6d5d2634e89f74df383
yanikisia <kathlyn.yary@gmail.com>
2022-02-11 22:39:52 -0300

    endpoint de pegar todos os produtos feito e testado

--- modulo4/LabECommerce/src/index.ts
+++ modulo4/LabECommerce/src/index.ts
@@ -2,6 +2,9 @@ import { app } from "./app";
 import { createUsers } from "./endpoints/createUsers";
 import {getAllUsers} from "./endpoints/getAllUsers"
 import {createProducts} from "./endpoints/createProducts"
+import { getAllProducts } from "./endpoints/getAllProducts";
+
 app.post("/users",createUsers);
 app.post("/products",createProducts);
 app.get("/users",getAllUsers);
+app.get("/products",getAllProducts);
